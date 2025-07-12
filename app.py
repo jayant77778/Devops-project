cc85f54881176e76b0a8580610597d3360044619
jayant77778 <163716243+jayant77778@users.noreply.github.com>
2025-07-12 15:17:23 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/info")
+def lwinfo():
+	return "This is Jayant Bhati"
+
+@app.route("/phone")
+def lwphone():
+	return "99911312310"
+
+app.run(host="0.0.0.0")
